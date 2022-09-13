fd47b48cab5a7f4ff79108024cbcabd33d1ee208
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-09-13 09:44:38 +0200

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
 import svgr from 'vite-plugin-svgr'
-import { visualizer } from 'rollup-plugin-visualizer'
+import {visualizer} from 'rollup-plugin-visualizer'
 import qiankun from 'vite-plugin-qiankun'
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({mode}) => ({
   plugins: [
     svgr(),
     preact(),
-    qiankun('orchy-preact-js-typescript-template', { useDevMode: true }),
+    qiankun('orchy-preact-js-typescript-template', {useDevMode: true}),
     visualizer()
   ],
   base: mode === 'development' ? `http://localhost:${port}/` : '/orchy-preact-js-typescript-template/',
-  server: { port },
+  server: {port, cors: true},
   test: {
     environment: 'jsdom',
     mockReset: true
