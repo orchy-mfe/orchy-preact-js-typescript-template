cd7e35a5d085b00feb0808832f12158fefc4699e
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-09-19 22:50:09 +0200

    feat: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import {render} from 'preact'
 import {renderWithQiankun, qiankunWindow, QiankunProps} from 'vite-plugin-qiankun/dist/helper'
+import {BrowserRouter} from 'react-router-dom'
 import {App} from './app'
 import './index.css'
 
 const retrieveContainer = (props: QiankunProps) => (props.container ?? document).querySelector('#app') as HTMLElement
 
 const renderApp = (props = {}) => {
-    render(<App />, retrieveContainer(props))
+    render(
+        <BrowserRouter basename={props.baseUrl}>
+            <App />, 
+        </BrowserRouter>,
+        retrieveContainer(props)
+    )
 }
 
 renderWithQiankun({
